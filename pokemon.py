98796a364aa46d006385ff40bf8cee5b5fedb5ba
Jerlon Buenconsejo <buenconsejo.jerlon@gmail.com>
2022-12-30 06:30:43 +0800

    Create pokemon.py

---
+++ pokemon.py
@@ -0,0 +1,27 @@
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import random
+import pandas as pd
+
+pyo.init_notebook_mode()
+
+pokemon = pd.read_csv('../DataAnalysis/data/pokemon.csv')
+x_values = pokemon['Type 1']
+y_values = pokemon['HP']
+data = [go.Scatter(x=x_values, y=y_values, mode='markers', marker = dict(
+    size=10, 
+    color='rgb(3,9,9)', 
+    symbol='hexagon2', 
+    line={'width':1}
+))]
+
+layout = go.Layout(
+    title = 'Pokemon Type and HP Correlation',
+    xaxis = dict(title = 'Type'),
+    yaxis = dict(title='Health Points'),
+    hovermode = 'closest'
+)
+
+fig = go.Figure(data=data, layout = layout)
+
+pyo.iplot(fig)
